700747031dcb3d84fac791f04d5bb2a1f63ef49e
borisrosedev <boris.rose.dev@gmail.com>
2026-08-24 14:39:23 +0200

    feat: makefile

---
+++ make/common.mk
@@ -0,0 +1,19 @@
+.ONESHELL:
+SHELL := /bin/bash
+.SHELLFLAGS := -eu -o pipefail -c
+.DEFAULT_GOAL := help
+# ====================================================
+INFO_COLOR := \033[36;1m
+UI_COLOR := \033[35;1m
+ERROR_COLOR := \033[31;1m
+SUCCESS_COLOR := \033[32;1m
+WARNING_COLOR := \033[33;1m
+RESET_COLOR := \033[m
+# ====================================================
+
+
+help: ## shows this help
+	@echo -e "\n$(UI_COLOR)=================MENU===========================$(RESET_COLOR)\n"
+	@grep -hE "^[a-zA-Z_.-]+:.*?## .*$$" $(MAKEFILE_LIST) | sort | \
+		awk 'BEGIN {FS=":.*?##"}{printf "$(INFO_COLOR)%-20s$(RESET_COLOR)%s\n", $$1, $$2}'
+	@echo -e "\n$(UI_COLOR)==================END OF MENU================$(RESET_COLOR)\n"
